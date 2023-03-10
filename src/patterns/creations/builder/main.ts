0b66e134592c43e47ab31a2bb25975186eabea40
Sangwoo Joo <nooni85@naver.com>
2023-03-10 23:52:31 +0900

    업데이트: 빌더 패턴

---
+++ src/patterns/creations/builder/main.ts
@@ -0,0 +1,85 @@
+class House {
+    parts: String[] = [];
+
+    public addParts(part: String): void {
+        this.parts.push(part);
+    }
+
+    public printParts(): void {
+        console.log(this.parts);
+    }
+}
+
+interface HouseBuilder {
+    buildSwimmingPool(): HouseBuilder;
+    buildWalls(): HouseBuilder;
+    buildDoors(): HouseBuilder;
+    buildWindows(): HouseBuilder;
+    buildRoof(): HouseBuilder;
+    buildGarage(): HouseBuilder;
+    getHouse(): House;
+}
+
+class HugeHouseBuilder implements HouseBuilder {
+    house: House = new House();
+
+    buildDoors(): HouseBuilder {
+        this.house.addParts("Doors");
+        return this;
+    }
+
+    buildGarage(): HouseBuilder {
+        this.house.addParts("Garage");
+        return this;
+    }
+
+    buildRoof(): HouseBuilder {
+        this.house.addParts("Roof");
+        return this;
+    }
+
+    buildSwimmingPool(): HouseBuilder {
+        this.house.addParts("SwimmingPool");
+        return this;
+    }
+
+    buildWalls(): HouseBuilder {
+        this.house.addParts("Walls");
+        return this;
+    }
+
+    buildWindows(): HouseBuilder {
+        this.house.addParts("Windows");
+        return this;
+    }
+
+    getHouse(): House {
+        return this.house;
+    }
+}
+
+class Director {
+    private builder: HouseBuilder;
+
+    constructor(builder: HouseBuilder) {
+        this.builder = builder;
+    }
+
+    public getHugeHouse(): House {
+        return new HugeHouseBuilder()
+            .buildGarage()
+            .buildDoors()
+            .buildDoors()
+            .getHouse();
+    }
+}
+
+function clientCode() {
+    const builder = new HugeHouseBuilder();
+    const director = new Director(builder);
+
+    const house = director.getHugeHouse();
+    house.printParts();
+}
+
+clientCode();
